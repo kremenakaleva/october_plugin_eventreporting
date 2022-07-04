fff2fb6a92fa75db04b7a8fd9c29d9cc5e42fc86
root <root@project-dev.pensoft.local>
2022-07-04 13:26:03 +0300

    new field

---
+++ updates/builder_table_update_pensoft_eventreporting_data_3.php
@@ -0,0 +1,27 @@
+<?php namespace Pensoft\EventReporting\Updates;
+
+use Schema;
+use October\Rain\Database\Updates\Migration;
+
+class BuilderTableUpdatePensoftEventreportingData3 extends Migration
+{
+    public function up()
+    {
+        Schema::table('pensoft_eventreporting_data', function($table)
+        {
+            $table->dateTime('event_end')->nullable(false)->unsigned(false)->default(null)->comment(null);
+            $table->dateTime('event_date')->nullable(false)->unsigned(false)->default(null)->comment(null)->change();
+            $table->string('website', 255)->nullable()->change();
+        });
+    }
+
+    public function down()
+    {
+        Schema::table('pensoft_eventreporting_data', function($table)
+        {
+            $table->dropColumn('event_end');
+            $table->date('event_date')->nullable(false)->unsigned(false)->default(null)->comment(null)->change();
+            $table->string('website', 255)->nullable(false)->change();
+        });
+    }
+}
